43c6a5bb70aac470408cb5f19e17aec31152443a
bethere server <james.b.pollack@gmail.com>
2012-01-26 04:16:11 +0000

    allofeverything

--- twitter_node/twit.js
+++
@@ -1,9 +0,0 @@
-var twitter = require('ntwitter');
-
-    var twit = new twitter({
-      consumer_key: '',
-      consumer_secret: '',
-      access_token_key: '',
-      access_token_secret: ''
-    });
-    
