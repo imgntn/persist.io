2be25c045be339f32aab7155a74b64ca5e2f32b5
James B. Pollack <jamesbradenpollack@gmail.com>
2012-02-01 18:40:26 +0000

    add

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var http = require('http')
 ss.client.define('main', {
   view:   'app.jade',
   css:    ['libs', 'app.styl'],
-  code:   ['libs/jquery.min.js', 'modules', 'main']
+  code:   ['libs', 'modules', 'main']
 });
 
 ss.http.router.on('/', function(req, res) {
