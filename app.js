a3ff35f9d099e6f6574def8e27e45b7a88785907
miningold <rynldsrap@gmail.com>
2012-02-23 12:12:19 -0800

    updated app.js to start console as module

--- app.js
+++ app.js
@@ -18,15 +18,19 @@ ss.client.formatters.add(require('ss-coffee'));
 ss.client.formatters.add(require('ss-jade'));
 ss.client.formatters.add(require('ss-stylus'));
 
+// Use server-side compiled Hogan (Mustache) templates. Others engines available
+ss.client.templateEngine.use(require('ss-hogan'));
+
 // redis 
 ss.session.store.use('redis', {host: '50.18.154.76', port: 6379});
 ss.publish.transport.use('redis', {host: '50.18.154.76', port: 6379});
 
-// Use server-side compiled Hogan (Mustache) templates. Others engines available
-//ss.client.templateEngine.use(require('ss-hogan'));
-
 // Minimise and pack assets if you type  SS_ENV=production node app.js
 if (ss.env == 'production') ss.client.packAssets();
+
+// Enable optional console server access. Run 'ss-client' to connect
+var consoleServer = require('ss-console').init(ss);
+consoleServer.listen(5000);
 
 var server = http.Server(ss.http.middleware);
 
