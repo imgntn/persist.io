0521b1b494d960e167e326c863702aaae1655912
miningold <rynldsrap@gmail.com>
2012-02-23 12:04:07 -0800

    updated socketstream and the proper files to get things running smoothly

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo nodemon -w . -w server/rpc/ -w server/middleware -w client/code/main/ -w client/code/modules/ app.js
+sudo nodemon -w . -w server/rpc/ -w server/middleware app.js
 
 
