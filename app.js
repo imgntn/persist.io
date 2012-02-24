b4fb6dad642c30eea08de5af75b5def988dc8efb
miningold <rynldsrap@gmail.com>
2012-02-24 03:57:35 -0800

    Added a simple everyauth twitter example

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 // My SocketStream app
 
-var http = require('http')
-  , ss = require('socketstream');
+var http      = require('http'),
+    ss        = require('socketstream'),
+    everyauth = require('everyauth'),
+    util      = require('util'),
+    redis     = require('redis'),
+    
+    client    = redis.createClient(6379, "50.18.154.76");
+    
+client.select(1);
 
 ss.client.define('main', {
   view: 'app.jade',
@@ -12,6 +19,56 @@ ss.client.define('main', {
 ss.http.router.on('/', function(req, res) {
   res.serve('main');
 });
+
+everyauth.twitter
+  .consumerKey('7i1XvDQqFSaQrMcjGcQPAg')
+  .consumerSecret('zQ14upSmunxpZpkaoPt3vT0yoKBB2HhmMuAG4pCqto')
+  .findOrCreateUser( function (session, accessToken, accessTokenSecret, twitterUserMetadata) {  
+    var user, newData, promise;
+    
+    promise = this.Promise();
+    
+    // Check for preexisting suer
+    client.get(twitterUserMetadata.id_str, function(err, data) {
+      if (err) {
+        console.log("Error accessing redis with username", err);
+        promise.fail(err);
+        return;
+      } 
+      
+      // User already exists
+      if (data) {
+        user = JSON.parse(data);
+        console.log("User exists:", util.inspect(user));
+        promise.fulfill(user);
+        return;
+      } else {
+        // User doesn't exist so we need to make a new one
+        user = {
+          accessToken: accessToken,
+          accessTokenSecret: accessTokenSecret,
+          name: twitterUserMetadata.name,
+          twitterId: twitterUserMetadata.id
+        };
+        
+        newData = JSON.stringify(user);
+        
+        client.set(twitterUserMetadata.id_str, newData, function(err, data) {
+          if (err) {
+            console.log("Error setting twitter user data in redis", err);
+            promise.fail(err);
+            return;
+          }
+          
+          console.log("User created:", util.inspect(user));
+          promise.fulfill(user);
+        });
+      }
+    });
+    
+    return promise;
+  })
+  .redirectPath('/');
 
 // Remove to use only plain .js, .html and .css files if you prefer
 ss.client.formatters.add(require('ss-coffee'));
@@ -31,6 +88,9 @@ if (ss.env == 'production') ss.client.packAssets();
 // Enable optional console server access. Run 'ss-client' to connect
 var consoleServer = require('ss-console').init(ss);
 consoleServer.listen(5000);
+
+ss.http.middleware.prepend(ss.http.connect.bodyParser());
+ss.http.middleware.append(everyauth.middleware());
 
 var server = http.Server(ss.http.middleware);
 
