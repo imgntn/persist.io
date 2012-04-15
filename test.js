3898aca96a499ad289c55f009686dd22992821ef
James Pollack <jamesbradenpollack@gmail.com>
2012-04-15 00:22:01 -0700

    msg2

--- test.js
+++ test.js
@@ -2,14 +2,17 @@ var redis   = require("redis");
 
 var msg = {
   "channels": ["channel1"],
-  "data": "are you listening?"
+  "data": "200"
+
+};
+
+var msg2 = {
+  "channels": ["channel1"],
   "data2": [400,400,400]
 
 };
 
-
-
 var client = redis.createClient();
 client.publish("juggernaut", JSON.stringify(msg));
-
+client.publish("juggernaut", JSON.stringify(msg2));
 
