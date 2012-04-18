ef396349a355ae944be1787081dd2eab62185644
James Pollack <jamesbradenpollack@gmail.com>
2012-04-17 22:57:52 -0700

    after ajax hell, figured out that JSON.parse cant handle length <= for SMEMBERS, since undefined comes across as a string instead of just null.  so you have to remove that last item.  that took fucking forever.

--- onDeath.js
+++ onDeath.js
@@ -1,4 +1,4 @@
-
+/*
 function onDeath () {
 
 
@@ -11,47 +11,59 @@ function addDeathTree()
 
 {
 	
-	$.get("http://bethere.io:7379/SADD/deathTrees/"+JSON.stringify(scene2treeContainer2.position))
+	$.get("http://bethere.io:7379/SADD/deathTrees/"+JSON.stringify(scene2treeContainer2.position));
 	
 }
+*/
 
 
 var deathTreeArrayFetch, deathTreeArrayParse, deathTreeArrayStaging;
 
+
+
 var deathTreeArray = [];
-
-
-
-function deathTreeArrayGet () {
-	
-	deathTreeArrayFetch = $.get("http://bethere.io:7379/SMEMBERS/deathTrees")
-	.success(function (){
-			
-			deathTreeArrayParse = JSON.parse(deathTreeArrayFetch.responseText);
-			deathTreeArrayStaging = deathTreeArrayParse.SMEMBERS;
-			deathTreeArray.push(deathTreeArrayStaging);
-			
-	deathTreeJSON();
-	
-			});
-	
-	
-}
-
-var i;
 var deathTreePositionArray = [];
-var temp;
 
-function deathTreeJSON()
-{
-	for (i=0; i <= deathTreeArrayStaging.length ; i++)
-		{
-			temp = JSON.parse(deathTreeArray[0][i]);
+var length, i, temp;
+/*	
+deathTreeArrayFetch = $.get("http://bethere.io:7379/SMEMBERS/deathTrees", function() {
+deathTreeArrayParse = JSON.parse(deathTreeArrayFetch.responseText);
+}).error(function() { console.log("error"); });
+
+deathTreeArrayFetch.success(function () { 
+	length = deathTreeArrayParse.SMEMBERS.length;
+		for (i=0; i<= length; i++){
+			
+temp=JSON.parse(deathTreeArrayParse.SMEMBERS[i]);
+			
 			deathTreePositionArray.push(temp);
+			console.log(temp);
+
+
 		}
-}
-
-
-
-
-
+	
+	 });
+	*/
+	
+ 	deathTreeArrayFetch = $.ajax({
+	  url: "http://bethere.io:7379/SMEMBERS/deathTrees",
+	  context: document.body,
+	dataType: "json",
+		  success: function(){
+	    deathTreeArrayParse =  JSON.parse(deathTreeArrayFetch.responseText);
+			length = deathTreeArrayParse.SMEMBERS.length;
+				for (i=0; i< length; i++){
+					temp = deathTreeArrayParse.SMEMBERS[i];
+		temp=JSON.parse(temp);
+				deathTreePositionArray.push(temp);
+					console.log(temp);
+	  }
+	}
+	
+	});
+	
+	/*
+	$(document).ajaxError(function(e, xhr, settings, exception) {
+	alert('error in: ' + settings.url + ' \n'+'error:\n' + xhr.responseText );
+	});
+	*/
