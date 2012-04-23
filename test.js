329cc9c7f277639b2efbec44dade87ef3bb9fcf5
James Pollack <jamesbradenpollack@gmail.com>
2012-04-22 23:23:25 -0700

    w00t!

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 var redis   = require("redis");
+
 var request = require('request');
-
-
 var msg = {
   "channels": ["channel1"],
   "data": "200"
@@ -26,8 +25,8 @@ client.publish("juggernaut", JSON.stringify(msg));
 client.publish("juggernaut", JSON.stringify(msg2));
 client.publish("juggernaut", JSON.stringify(msg3));
 
-request('http://bethere.io:7379/SMEMBERS/sessionIDs', function (error, response, body) {
+request('http://bethere.io:7379/GET/nonuniquevisitors', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log(body) // Print the google web page.
-  };
-
+  }
+});
