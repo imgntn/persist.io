a59a48f7a30a57d28eac97786eeec2d13b4f8319
James Pollack <jamesbradenpollack@gmail.com>
2012-04-22 23:29:36 -0700

    w00t!

--- test.js
+++ test.js
@@ -25,14 +25,24 @@ client.publish("juggernaut", JSON.stringify(msg));
 client.publish("juggernaut", JSON.stringify(msg2));
 client.publish("juggernaut", JSON.stringify(msg3));
 
-request1 = request('http://bethere.io:7379/GET/nonuniquevisitors', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) // Print the google web page.
-  }
-});
 
-request2 = request('http://bethere.io:7379/SMEMBERS/sessionIDs', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) // Print the google web page.
-  }
-});
+var requests = [];
+var url;
+url = 'http://bethere.io:7379/GET/nonuniquevisitors'
+requests.push(url);
+url='http://bethere.io:7379/SMEMBERS/sessionIDs';
+requests.push(url);
+
+for (var i=0; i < requests.length; i++){
+	
+	request[i] = request(requests[i], function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    console.log(body) // Print the google web page.
+	  }
+	});
+	
+	
+};
+
+
+
